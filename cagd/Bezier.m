d7637f0747067e867a086d5163a1098f660e3f94
Jure Slak <jure.slak@gmail.com>
2017-03-17 09:59:21 +0100

    Subdivision and degree elevation.

--- cagd/Bezier.m
+++ cagd/Bezier.m
@@ -7,7 +7,7 @@ classdef Bezier
     methods
         function obj = Bezier(B)
             % B = seznam koordinat kontrolnih tock, vrstice so tocke
-            if ~ismatrix(B), error('B is not a matrix!'), end
+            assert(ismatrix(B), 'B is not a matrix!');
             obj.B = B;
             [obj.n, obj.d] = size(B);          
             obj.n = obj.n - 1;
@@ -15,7 +15,7 @@ classdef Bezier
         function D = decastejau(self, t, dim)
             % t = value of parameter
             % dim = for which coordinate to create the scheme
-            if dim < 0 || dim > self.d, error('dim must be between 1 and %d.', self.d), end
+            assert(0 <= dim <= self.d, 'dim must be between 1 and %d.', self.d);
             D = zeros(self.n, self.n+1);
             for j = 1:self.n+1
                 D(j, 1) = self.B(j, dim);
@@ -28,7 +28,7 @@ classdef Bezier
         end
         function b = val(self, t)
             % t = vector of values for parameter
-            if nargin < 2, error('Supply values for patameter t.'), end
+            assert(nargin == 2, 'Supply values for patameter t.');
             p = length(t);
             b = zeros(p, self.d);
             for j = 1:p
@@ -41,8 +41,8 @@ classdef Bezier
         function bd = der(self, t, r)
             % returns values of r-th derivatives at times t 
             if nargin < 3, r = 1; end
-            if nargin < 2, error('Supply values for patameter t.'), end
-            if r < 0, error('r must be integer in [0, inf)'), end
+            assert(nargin >= 2, 'Supply values for patameter t.');
+            assert(r >= 0, 'r must be integer in [0, inf)');
             p = length(t);
             bd = zeros(length(t), self.d);
             if r > self.n, return, end
@@ -56,10 +56,48 @@ classdef Bezier
             if r > self.n, bdc = []; return, end
             bdc = self.facdiff(self.B, r, self.n);
         end
+        function curvelist = subdivide(self, t, k)
+            % returns a list of control polygons for 2^k parts of t:(1-t) division
+            if nargin < 3, k = 1; end
+            assert(isscalar(t), 't must be scalar.');
+            curvelist = cell(1, 1);
+            curvelist{1} = self.B;
+            for level = 1:k
+                curvelist_new = cell(2^level, 1);
+                for j = 1:2^(level-1)
+                    left = zeros(self.n+1, self.d);
+                    right = zeros(self.n+1, self.d);
+                    Subd = Bezier(curvelist{j});
+                    for i = 1:self.d
+                        D = Subd.decastejau(t, i);
+                        left(:, i) = D(1, :);
+                        right(:, i) = D(end-self.n:-self.n:self.n-1);
+                    end
+                    curvelist_new{2*j-1} = left;
+                    curvelist_new{2*j} = right;
+                end
+                curvelist = curvelist_new;
+            end
+        end
+        function BE = elevate(self, k)
+            % elevate degree by k
+            if nargin < 2, k = 1; end
+            assert(k >= 0, 'k must be nonnegative.');
+            BE = self.B;
+            for i = 1:k
+                BE_new = zeros(self.n+i+1, self.d);
+                BE_new(1, :) = BE(1, :);
+                BE_new(self.n+i+1, :) = BE(self.n+i, :);
+                for j = 1:self.n+i-1
+                    BE_new(j+1,:) = (1-j/(self.n+i)) * BE(j+1, :) + j/(self.n+i) * BE(j, :);
+                end
+                BE = BE_new;
+            end
+        end
         function plot(self, npts)
             % plots bezier curve using n equally spaced points
             if nargin < 2, npts = 100; end
-            if npts < 2, error('Not enough points to plot the curve.'), end
+            assert(npts >= 2, 'Not enough points to plot the curve.');
             t = linspace(0, 1, npts);
             b = self.val(t);
             hold on
@@ -78,8 +116,8 @@ classdef Bezier
             % plots bezier curve using n equally spaced points
             if nargin < 3, nder = 20; end
             if nargin < 2, npts = 100; end
-            if npts < 2, error('Not enough points to plot the curve.'), end
-            if self.d ~= 2, error('Only for 2D.'), end
+            assert(npts >= 2, 'Not enough points to plot the curve.');
+            assert(self.d == 2, 'Only for 2D.');
             t = linspace(0, 1, npts);
             tder = linspace(0, 1, nder);
             b = self.val(t);
--- cagd/beziertest.m
+++ cagd/beziertest.m
@@ -4,11 +4,15 @@ clear
 %B = Bezier(kont);
 %B.plot(100)
 
-figure
-kont2 = [0, 0; 2, 1; 3, -2; 5, 3];
+% figure
+kont2 = [
+    0, 0; 
+    2, 3; 
+    4, 2; 
+    5, -1];
 B2 = Bezier(kont2);
-B2.plot(100)
-B2.plotwithder()
+% B2.plot(100)
+% B2.plotwithder()
 
 kontder0 = B2.dercontrol(0);
 kontder1 = B2.dercontrol(1);
@@ -20,8 +24,23 @@ Bder2 = Bezier(kontder2);
 t = 0:1/10:1;
 der2vals = Bder2.val(t);
 der2vals_alt = B2.der(t, 2);
-if norm(der2vals-der2vals_alt, 'inf') > 1e-13, error('Two ways of calculating derivatives do not match.'), end
+assert(norm(der2vals-der2vals_alt, 'inf') < 1e-13, 'Two ways of calculating derivatives do not match.');
 
+blist = B2.subdivide(1/2, 0);
+assert(length(blist) == 1, 'Subdivide for 0 returned too many items.');
+assert(norm(blist{1} - B2.B) < 1e-16, 'Subdivision for 0 is wrong.');
+blist = B2.subdivide(1/2, 2);
+% for i = 1:length(blist)
+%     bz = Bezier(blist{i});
+%     bz.plot();
+% end   
+
+BE = B2.elevate(2);
+Belevated = Bezier(BE);
+
+eleval = Belevated.val(0:0.1:1);
+normval = B2.val(0:0.1:1);
+assert(norm(eleval-normval) < 1e15, 'Elevation is wrong');
 
 %figure
 %kont3 = [0, 0, 4; 5, -6, 1; 2, -2, -2; 6, 3, 8];
